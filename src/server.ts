46eeb1209de9719ad56e2fb969f071b134bb2b69
tarikcelik1 <147607115+tarikcelik1@users.noreply.github.com>
2024-01-06 07:07:54 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -12,62 +12,13 @@ import { Server } from "socket.io";
 import http from "http";
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log(`User connected ${socket.id}`);
-
-  socket.on("join_room", (data) => {
-    socket.join(data.room); // Join the user to a socket room
-    console.log(
-      `user:${socket.id} user_id: ${data.user},user_name: ${data.user_name}, user_room: ${data.room}`
-    );
-  });
-  socket.on("join_not", (data) => {
-    socket.join("notification"); // Join the user to a socket room
-    console.log(
-      `proposed_user_id:${data.notification.proposed_user_id} , user_id: ${data.user},user_name: ${data.user_name}`
-    );
-  });
-  socket.on("join_camp", (data) => {
-    socket.join("camp"); // Join the user to a socket room
-    console.log(
-      `Proposal Created:campaign_id:${data.notification} , user_id: ${data.user},user_name: ${data.user_name}`
-    );
-  });
-  socket.on("send_camp", (data) => {
-    //{ user: user, message: message, room: 'room1' }
-    io.to("camp").emit("receive_not", data);
-    console.log(
-      `proposal emitted : user:${socket.id} campaign_id: ${data.campaign_id}, message: ${data.proposed_user_id},status : ${data.status} , campaign_header: ${data.user_name} `,
-    );
-  });
-  socket.on("send_notification", (data) => {
-    //{ user: user, message: message, room: 'room1' }
-    io.to("notification").emit("receive_notification", data);
-    console.log(
-      `user:${socket.id} user_name: ${data.user}, message: ${data.proposed_user_id},status : ${data.status} , campaign_header: ${data.campaign_header} `,
-    );
-  });
-  socket.on("send_message", (data) => {
-    //{ user: user, message: message, room: 'room1' }
-    io.to(data.room).emit("receive_message", data);
-    console.log(
-      `user:${socket.id} user_name: ${data.user}, message: ${data.message}`,
-      data.room
-    );
-  });
-});
-
-server.listen(process.env.PORT2, () => {
-  console.log("Welcome to Tata-MESSAGE");
-});
+const corsOptions = {
+  origin: ["http://localhost:3000/", "null"], // Add 'null' as an allowed origin
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // Enable cookies and HTTP authentication if needed
+  optionsSuccessStatus: 204, // HTTP status code to respond with for preflight requests
+};
+app.use(cors(corsOptions));
 
 
 app.use(morgan("dev"));
