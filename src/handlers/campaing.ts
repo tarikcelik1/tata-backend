d2412042e9f97a41b636c541479c1f4636309465
tarikcelik1 <147607115+tarikcelik1@users.noreply.github.com>
2024-01-21 20:10:29 +0300

    Update campaing.ts

--- src/handlers/campaing.ts
+++ src/handlers/campaing.ts
@@ -171,7 +171,6 @@ export const deleteCampaign = async (req, res) => {
             preffered_platforms: true,
           },
         },
-        collaboration: true,
         proposal: true,
         rating: true,
       },
